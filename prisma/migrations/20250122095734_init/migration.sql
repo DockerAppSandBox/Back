e6e887114c86f20adbc4a1eeecf24f99e8948215
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2025-01-22 10:58:41 +0100

    update the migration (id user is now uuid )

--- prisma/migrations/20250122095734_init/migration.sql
+++ prisma/migrations/20250122095734_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
