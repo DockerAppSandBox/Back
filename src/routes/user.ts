b0c45cb7511f7692c7775ae16c6e1e8ef10f2384
ClaireF <claire.fernandes.ee@gmail.com>
2025-01-24 10:52:31 +0100

    delete unused root

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,11 +3,7 @@ import UserController from "../controllers/user";
 
 const Router = express.Router();
 
-Router.post("/", UserController.createUser);
-Router.get("/:id", UserController.getUserById);
-Router.get("/", UserController.getAllUsers);
 Router.put("/:id", UserController.updateUser);
 Router.delete("/:id", UserController.deleteUser);
-
 
 export default Router;
