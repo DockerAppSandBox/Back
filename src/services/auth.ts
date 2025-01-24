f8e8ef9394182c385c27e748fb7996decc063be9
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2025-01-24 11:44:36 +0100

    update tthe auth service

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,21 +1,20 @@
-import bcrypt from 'bcrypt';
 import prisma from '../config/database';
 import { RegisterUserDTO, LoginUserDTO, AuthResponse } from '../entity/auth';
 import UserService from './user';
 import generateToken from '../utils/auth';
 import { NotFoundError, BadRequestError } from '../http_code/error-code';
+import { hashPassword, verifyPassword } from '../utils/security';
 
 export class AuthService {
   static async register(data: RegisterUserDTO): Promise<AuthResponse> {
     const existingUser = await prisma.user.findUnique({ where: { email: data.email } });
     if (existingUser) throw new BadRequestError('Email already in use');
 
-    const hashedPassword = await bcrypt.hash(data.password, 10);
+    const hashedPassword = await hashPassword(data.password)
     const newUser = await UserService.createUser({
       email: data.email,
       password: hashedPassword,
     });
-
     const token = generateToken({ id: newUser.id});
     return { user: { id: newUser.id, email: newUser.email }, token };
   }
@@ -24,7 +23,7 @@ export class AuthService {
     const user = await prisma.user.findUnique({ where: { email: data.email } });
     if (!user) throw new NotFoundError('Invalid email or password');
 
-    const isValidPassword = await bcrypt.compare(data.password, user.password);
+    const isValidPassword = await verifyPassword(data.password, user.password)
     if (!isValidPassword) throw new BadRequestError('Invalid email or password');
 
     const token = generateToken({ id: user.id});
